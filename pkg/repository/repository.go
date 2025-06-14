e8bb6110b3c6b509fef3dbf9754d5e03dfb2a0a9
Dorkhan1 <dorkhanaidar@gmail.com>
2025-06-14 16:10:33 +0500

    Daily commit

---
+++ pkg/repository/repository.go
@@ -0,0 +1,26 @@
+package repository
+
+import "github.com/Dorkhan1/todo-app/pkg/repository"
+
+type Authorization interface {
+
+}
+
+type TodoList interface {
+
+}
+
+type TodoItem interface {
+
+}
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository(repos *repository.Repository) *Repository {
+	return &Repository{
+
+	}
+}
